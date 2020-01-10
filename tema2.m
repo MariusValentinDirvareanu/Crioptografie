fc64e46e3af7df4755d139bf91d6be55e38c85e7
mariu <dirvareanumarius@live.com>
2020-01-10 12:44:35 +0200

    Tema 2

--- tema2.m
+++ tema2.m
@@ -1,6 +1,17 @@
 clc; clear all; close all;
+% Autor : Dirvareanu Marius-Valentin
+% Facultatea si grupa : FSA 1341a
+% An : 2020
 % Sistemul de criptare Vigenere
+% FSA (cheie)-> SUNT STUDENT IN ANUL PATRU LA FACULTATEA DE STIINTE
+% APLICATE (mesaj)
+% Toate mesajele decriptate si cheile corespunzatoare sunt salvate in
+% fisierul mesaje.txt
 fileID = fopen('mesaje.txt','w');
+% Se seteaza cheia initiala ca si 'AAA', dupa care se incremenzeaza pe rand
+% fiecare litera incepand cu ultima pozitie. La incrementarea literei de pe
+% pozitia 2 se reseteaza litera de pe pozitia 3, iar la incrementarea
+% literei de pe pozitia 1 se reseteaza literele de pe pozitiile 2 si 3
 keynum='AAA'-double('A');
 mesaj='XMNYKTZVESLISSNZDPFLRZDAKSCZDTFLEFVEXLINFTJSPQACFLE';
 mesajnum=mesaj-double('A');
@@ -14,7 +25,12 @@ for x=1:26
                 rezultat(i)=mesajnum(i)-keynumVec(i);
             end
             rezultat=mod(rezultat,26)+double('A');
+            % Se exclud cuvintele care contin X, W, Y, Q, K, deoarece nu
+            % prea sunt intalnite in limba romana (am presupus ca mesajul este in limba romana)
             if contains(char(rezultat),'W')==0&&contains(char(rezultat),'X')==0&&contains(char(rezultat),'Y')==0&&contains(char(rezultat),'Q')==0&&contains(char(rezultat),'K')==0
+                % daca conditia este indeplinita, atunci se scriu in
+                % fisierul mesaje.txt cheia si mesajul decriptat de forma
+                % cheie -> mesaj
                 fprintf(fileID,'%s -> %s\n',char(keynum+double('A')),char(rezultat));
             end
             keynum(3)=keynum(3)+1;
@@ -27,4 +43,5 @@ for x=1:26
     keynum(2)=0;
     keynum(1)=keynum(1)+1;
 end
+% Se inchide fisierul mesaje.txt
 fclose(fileID);
